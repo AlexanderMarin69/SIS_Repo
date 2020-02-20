56ecabcbb2257f190a8a41476c2d7384029605c1
Alexander Marin <maal18en@student.ju.se>
2020-02-20 10:32:00 +0100

    test

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,23 @@
 module.exports = {
-  "outputDir": "wwwroot",
-  "publicPath": "/",
-  "transpileDependencies": [
-    "vuetify"
-  ]
+    "outputDir": "wwwroot",
+    "publicPath": "/",
+    "transpileDependencies": [
+        "vuetify"
+    ],
+
+    //dev: {
+    //    env: require('./dev.env'),
+    //    port: 44333,
+    //    autoOpenBrowser: true,
+    //    assetsSubDirectory: 'static',
+    //    assetsPublicPath: '/',
+    //    proxyTable: {},
+    //    cssSourceMap: false
+    //},
+    //proxyTable: {
+    //    '/api': {
+    //        target: 'http://localhost:44333',
+    //        changeOrigin: true
+    //    }
+    //},
 }
